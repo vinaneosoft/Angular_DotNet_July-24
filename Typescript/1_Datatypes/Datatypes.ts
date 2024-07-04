15e2a56ecf9bbc398b9dd78af08f909399ab3de3
vina <vinaneosoft@gmail.com>
2024-07-04 15:38:35 +0530

    Initial commit

---
+++ Typescript/1_Datatypes/Datatypes.ts
@@ -0,0 +1,20 @@
+let department;// no type decided
+department=123; //number  
+console.log(typeof department);
+department="LD"; // string   
+console.log(typeof department);
+department=true; // boolean  
+console.log(typeof department);
+var address;
+const pie=3.142;
+
+//strict types
+let empId:number   // type annotations, type is known in prior
+empId=123;
+console.log(typeof empId);
+//empId="3445";
+
+let companyName:string;
+companyName="Neosoft";
+console.log(typeof companyName);
+//companyName=0;
