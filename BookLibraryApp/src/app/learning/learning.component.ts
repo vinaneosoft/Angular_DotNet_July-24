9cff6712aa0cecb7bb2bc56589f0eb603649a76c
vina <vinaneosoft@gmail.com>
2024-07-10 15:22:59 +0530

    commit

--- BookLibraryApp/src/app/learning/learning.component.ts
+++ BookLibraryApp/src/app/learning/learning.component.ts
@@ -10,6 +10,16 @@ export class LearningComponent {
     chapterName="Data Binding" // data
     addResult=0;
     age=0;
+
+    bookNames=[
+      'Learn Java in Easy way', 
+      'Basics of Java', 
+      'Web Basics All in one', 
+      'Fundamentals of Python',
+      'Java in Detail'
+    ]
+
+
     constructor(){
       setTimeout(()=>{
         this.chapterName="1 Way and 2 Way Data Binding"
