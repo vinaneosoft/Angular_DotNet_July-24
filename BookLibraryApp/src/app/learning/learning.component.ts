286d83336f5f6ffbabd3ecbb9398c5439bcce71c
vina <vinaneosoft@gmail.com>
2024-07-10 14:32:01 +0530

    commit

--- BookLibraryApp/src/app/learning/learning.component.ts
+++ BookLibraryApp/src/app/learning/learning.component.ts
@@ -9,28 +9,29 @@ import { FormsModule } from '@angular/forms';
 export class LearningComponent {
     chapterName="Data Binding" // data
     addResult=0;
+    age=0;
     constructor(){
       setTimeout(()=>{
         this.chapterName="1 Way and 2 Way Data Binding"
-        console.log(this.chapterName);
+       // console.log(this.chapterName);
       }, 5000);
-      console.log(this.chapterName);
+     // console.log(this.chapterName);
       
     }
     test():void{
-      console.log(this.chapterName);
+      //console.log(this.chapterName);
     }
     addNums(num1:any,num2:any){
-      console.log(num1);
-      console.log(typeof num1);
-      console.log(num2);
-      console.log(typeof num2);
+     // console.log(num1);
+    //  console.log(typeof num1);
+      //console.log(num2);
+    //  console.log(typeof num2);
       this.addResult=parseFloat(num1)+parseFloat(num2);
     }
     accessTemplate(template:any){
-      console.log(template);
+    //  console.log(template);
       console.log(typeof template);
-      console.log(template.innerText);
-      console.log(template.children);
+     console.log(template.innerText);
+     console.log(template.children);
     }
 }
