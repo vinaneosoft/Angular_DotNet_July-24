8aa56cd437b2bc4c03f638f6dbb38631e1e52cc3
vina <vinaneosoft@gmail.com>
2024-07-12 15:27:22 +0530

    commit

--- BookLibraryApp/src/app/books/books.component.ts
+++ BookLibraryApp/src/app/books/books.component.ts
@@ -18,9 +18,9 @@ export class BooksComponent {
   }
 
   books:TechnicalBook[]=[
-    new TechnicalBook("1","Learn Java", "Marlyn Dis", 560.79, new Date("2 June 2005"), "this is java book"),
-    new TechnicalBook("3","Basisc of Java", "Preesh kumar", 760.3, new Date("12 June 2005"), "this is java book"),
-    new TechnicalBook("2","Python in Easy Way", "Subhash chaturvedi", 700, new Date("2 May 2001"), "this is python book"),
+    new TechnicalBook("1","Learn Java", "Marlyn Dis", 560.79, new Date("2 June 2005"), "this is JAVA book"),
+    new TechnicalBook("3","Basics of java", "Preesh kumar", 760.3, new Date("12 June 2005"), "this is java book"),
+    new TechnicalBook("2","python in Easy Way", "Subhash chaturvedi", 700, new Date("2 May 2001"), "this is python book"),
     new TechnicalBook("4","Web Basics", "Krish sathe", 560.99, new Date("20 Aug 2004"), "this is web basics book")
   ]
 
