13d872e8ca5ff88852ada979488f92ea5c191137
vina <vinaneosoft@gmail.com>
2024-07-17 12:44:57 +0530

    commit

--- BookLibraryApp/src/app/book-input/book-input.component.ts
+++ BookLibraryApp/src/app/book-input/book-input.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TechnicalBook } from '../myclasses/technical-book';
 
 @Component({
   selector: 'app-book-input',
@@ -10,16 +11,17 @@ export class BookInputComponent {
 
   bookInputForm:FormGroup;
 
+  book=new TechnicalBook();
   constructor(){
     this.bookInputForm=new FormGroup({
-      id:new FormControl(""),
-      bookName:new FormControl(""),
-      bookAuthor:new FormControl(""),
-      bookPrice:new FormControl(""),
-      bookPublishDate:new FormControl(""),
-      bookDescription:new FormControl(""),
-      bookImage:new FormControl(""),
-      bookConfirmPrice:new FormControl("")
+      id:new FormControl("", [Validators.required]),
+      bookName:new FormControl("", [Validators.required]),
+      bookAuthor:new FormControl("",[Validators.required]),
+      bookPrice:new FormControl("", [Validators.required]),
+      bookPublishDate:new FormControl("", [Validators.required]),
+      bookDescription:new FormControl("", [Validators.required]),
+      bookImage:new FormControl("",[]),
+      bookConfirmPrice:new FormControl("", [Validators.required])
     });
   }
   collectData(){
@@ -28,7 +30,12 @@ export class BookInputComponent {
    //console.log(this.bookInputForm.get("id")); // to get individual form control
   // console.log(this.bookInputForm.value.id="45");
   // console.log(this.bookInputForm.value.bookName="Java");
-   console.log(this.bookInputForm.value);
-   
+   //console.log(this.bookInputForm.value);
+   this.book=this.bookInputForm.value; // if we have keys for both same then assignment is possible
+   console.log(this.book);
+   this.addBook();
+  }
+  addBook(){
+     // in http chapter we will send this book to backend via post request
   }
 }
